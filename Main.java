ae78e9934b657685fbcca1e66b60005b1985f397
syntonym <syntaar@gmail.com>
2017-03-10 13:38:57 +0100

    Add generator
    change to double
    add the data generator
    change some API details

--- Main.java
+++ Main.java
@@ -14,29 +14,41 @@ public class Main {
 
 	public static void main(String[] args) {
 		int dimensions = 2;
-		Point[] points = generateData(5, 200, dimensions);
+		Point[] points = generateData(4, 1000, dimensions);
 		algoKMeans(points, Initialisation.RANDOM_PARTITION, Strategy.LLOYD);
-		visualize(points);
+		visualize(points, Initialisation.RANDOM_PARTITION, Strategy.LLOYD);
 	}
 
 	/**
 	 * return generated point data (exercise part 1)
 	 *
-	 * @param clusters   number of clusters generated (K on the assignment sheet)
-	 * @param points     total number of points (N on the assignment sheet)
-	 * @param dimensions number of dimensions in which a single points lives (D on the assignment sheet)
+	 * @param clusters      number of clusters generated (K on the assignment sheet)
+	 * @param points        total number of points (N on the assignment sheet)
+	 * @param dimensions    number of dimensions in which a single points lives (D on the assignment sheet)
+	 * @param deviation_max maximum of deviation. Should be chosen in such a way that clusters overlap.
+	 * @param mean_max      maxiumum of mean. Should be chosen in such a way that clusters overlap.
 	 **/
-	public static Point[] generateData(int clusters, int points, int dimensions) {
+	public static Point[] generateData(int clusters, int points, int dimensions, double deviation_max, double mean_max) {
 		Point[] r_points = new Point[points];
 		Random r = new Random();
 		int cluster_id;
 		Point p;
 
+		if (deviation_max == 0.0) {
+			deviation_max = 33;
+		}
+		if (mean_max == 0.0) {
+			mean_max = 100;
+		}
+
 		double[][] cluster = new double[clusters][2];
+		double deviation;
+		double mean;
 
 		for (int i=0; i<cluster.length; i++) {
-			double deviation = r.nextDouble() * 50;
-			double mean = r.nextDouble() * 100;
+
+			deviation = r.nextDouble() * deviation_max;
+			mean = r.nextDouble() * mean_max;
 			cluster[i][0] = deviation;
 			cluster[i][1] = mean;
 		}
@@ -52,6 +64,10 @@ public class Main {
 		}
 
 		return r_points;
+	}
+
+	public static Point[] generateData(int clusters, int points, int dimensions) {
+		return generateData(clusters, points, dimensions, 0.0, 0.0);
 	}
 
 	/**
@@ -82,9 +98,14 @@ public class Main {
 	 * The visualisiation only needs to be 2D. You can use whatever
 	 * projection you like.
 	 *
-	 * @param points Array of points to visualise.
+	 * If the backend supports some kind of title include the initialisation
+	 * strategy and the update strategy in some kind.
+	 *
+	 * @param points         Array of points to visualise.
+	 * @param initialisation strategy to initialisation k-means algo
+	 * @param strategy       strategy to update k-means algo
 	 **/
-	public static void visualize(Point[] points) {
+	public static void visualize(Point[] points, Initialisation initialisation, Strategy strategy) {
 	}
 }
 
