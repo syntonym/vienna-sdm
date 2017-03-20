c3a860b0169262bfb179c928dfb40c29c7091769
Richi <ricardo.pablito@yahoo.de>
2017-03-20 15:23:15 +0100

    Testordner für Processing angelegt

---
+++ test/Visualizer.java
@@ -0,0 +1,10 @@
+//import processing.core.*;
+
+public class Visualizer //extends PApplet
+{
+
+    public static void main(String[] args) {
+        System.out.println("moin");
+//        PApplet.main("Visualizer");
+    }
+}
